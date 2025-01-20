1346396431f7bd5aec52c554f288629b1044d674
kedor <99351777+aure31@users.noreply.github.com>
2025-01-20 14:49:59 +0000

    game

--- game.py
+++ game.py
@@ -1,2 +1,21 @@
+from joueur import Joueur
 
+states={}
 
+class game:
+    def __init__(self,players:list):
+        self.file_influance = []
+        self.players = players
+        self.state = "start"
+
+    def start_game(self):
+        for e in self.players : 
+
+    def add_end(self,card):
+        self.file_influance.append(card)
+    
+    def add_start(self,card):
+        self.file_influance.insert(0,card)
+
+    def end_turn(self):
+        self.state = "end"
