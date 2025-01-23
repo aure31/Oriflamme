ab2cb3a72ed0b15bec5e38474ae064531aff4369
kedor <99351777+aure31@users.noreply.github.com>
2025-01-23 10:31:03 +0000

    update

--- cartes.py
+++ cartes.py
@@ -1,13 +1,27 @@
 import pygame as p
 
+
+
 class Types:
-    def __init__(self, nom, capacite, img_recto):
+    def __init__(self,id, nom,description):
+        self.id = id
         self.nom = nom
-        self.img_recto = img_recto
-        self.capa = capacite
+        self.description = description
+
+    def capacite(self):
+        raise NotImplementedError("The method not implemented")
+
+class Seigneur(Types):
+    def __init__(self):
+        super().__init__(0,"Seigneur", "gagne 1")
+
+    def capacite(self,Player,Game):
+        #Todo
+        return
+        
 
 class Carte(p.sprite.Sprite):
-    def __init__(self, type):
+    def __init__(self, type:Types):
         self.couleur = None
         self.type = type
         self.img = type
@@ -18,3 +32,6 @@ class Carte(p.sprite.Sprite):
         self.couleur = joueur.couleur
         return self
     
+    def capacite(self,game):
+        player = game.get_player(self.idPlayer)
+        self.type.capacite(player,game)
