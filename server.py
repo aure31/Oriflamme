cd786d2cd5329a10d0cdf532ea1d062efccb3fb7
ArmandNSI <armand.b@edr31.com>
2025-01-27 14:05:05 +0000

    Fichier serveur

---
+++ server.py
@@ -0,0 +1,42 @@
+import socket
+from _thread import *
+import sys 
+
+server = "10.0.1.22"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(5)
+print("En attente de connexion...")
+
+def threaded_client(conn):
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Déconnecté")
+                break
+            else:
+                print("Reçu : ", reply)
+                print("Envoi : ", reply)
+                conn.sendall(str.encode(reply))
+        except:
+            break
+    
+    print("Connexion perdue")
+    conn.close()
+
+while True:
+    conn, addr = s.accept()
+    print("Connecté à : ", addr)
+
+    start_new_thread(threaded_client, (conn,))
