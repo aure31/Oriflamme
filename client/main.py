9f7335965042c6f0894c041a9bfa369c30a33b85
ArmandNSI <abonnementsarba@laposte.net>
2025-01-31 19:24:09 +0100

    continuer

--- client/main.py
+++ client/main.py
@@ -7,7 +7,7 @@ from server.game import Game
 
 p.init()
 p.mixer.init()  # Initialize the mixer
-p.mixer.music.load("client/assets/fond_sonore.mp3")  # Load the music file
+p.mixer.music.load("client/assets/musiques/fond_sonore.mp3")  # Load the music file
 p.mixer.music.set_volume(0.3)  # Set volume (0.0 to 1.0)
 p.mixer.music.play(-1)  # Play the music in a loop
 
@@ -15,16 +15,18 @@ p.display.set_caption('Oriflamme')
 
 window = p.display.set_mode((0,0), p.FULLSCREEN)
 screen_width, screen_height = window.get_size()
-background = p.image.load("client/assets/bg_lobby.png").convert()
+background = p.image.load("client/assets/background/bg_lobby.png").convert()
 background_image = p.transform.scale(background, (screen_width, screen_height))
-join = p.image.load("client/assets/join.png")#.convert_alpha()
-join_touched = p.image.load("client/assets/join_touched.png")
-new_game = p.image.load("client/assets/new_game.png")#.convert_alpha()
-new_game_touched = p.image.load("client/assets/new_game_touched.png")
-settings = p.image.load("client/assets/settings.png")
-settings_touched = p.image.load("client/assets/settings_touched.png")
-credits = p.image.load("client/assets/credits.png")
-credits_touched = p.image.load("client/assets/credits_touched.png")
+join = p.image.load("client/assets/boutons/join.png")#.convert_alpha()
+join_touched = p.image.load("client/assets/boutons/join_touched.png")
+new_game = p.image.load("client/assets/boutons/new_game.png")#.convert_alpha()
+new_game_touched = p.image.load("client/assets/boutons/new_game_touched.png")
+settings = p.image.load("client/assets/boutons/settings.png")
+settings_touched = p.image.load("client/assets/boutons/settings_touched.png")
+credits = p.image.load("client/assets/boutons/credits.png")
+credits_touched = p.image.load("client/assets/boutons/credits_touched.png")
+quitter = p.image.load("client/assets/boutons/quit.png")
+quitter_touched = p.image.load("client/assets/boutons/quit_touched.png")
 
 def main():
     is_running = True
@@ -48,30 +50,35 @@ def main():
         
         if lobby_1:
             window.blit(background_image, (0, 0))
-            window.blit(join, (1000, 150))
-            window.blit(new_game, (1000, 300))
-            window.blit(settings, (1000, 450))
-            window.blit(credits, (1000, 600))
-            if join.get_rect(topleft=(1000, 150)).collidepoint(mouse_pos):
-                window.blit(join_touched, (1000, 150))
+            window.blit(join, (1000, 100))
+            window.blit(new_game, (1000, 250))
+            window.blit(settings, (1000, 400))
+            window.blit(credits, (1000, 550))
+            window.blit(quitter, (1000, 700))
+            if join.get_rect(topleft=(1000, 100)).collidepoint(mouse_pos):
+                window.blit(join_touched, (1000, 100))
                 if p.mouse.get_pressed()[0]:
                     lobby_1 = False
                     lobby_2 = True 
-            if new_game.get_rect(topleft=(1000, 300)).collidepoint(mouse_pos):
-                window.blit(new_game_touched, (1000, 300))
+            if new_game.get_rect(topleft=(1000, 250)).collidepoint(mouse_pos):
+                window.blit(new_game_touched, (1000, 250))
                 if p.mouse.get_pressed()[0]:
                     lobby_1 = False
                     lobby_3 = True
-            if settings.get_rect(topleft=(1000, 450)).collidepoint(mouse_pos):
-                window.blit(settings_touched, (1000, 450))
+            if settings.get_rect(topleft=(1000, 400)).collidepoint(mouse_pos):
+                window.blit(settings_touched, (1000, 400))
                 if p.mouse.get_pressed()[0]:
                     lobby_1 = False
                     sett = True
-            if credits.get_rect(topleft=(1000, 600)).collidepoint(mouse_pos):
-                window.blit(credits_touched, (1000, 600))
+            if credits.get_rect(topleft=(1000, 550)).collidepoint(mouse_pos):
+                window.blit(credits_touched, (1000, 550))
                 if p.mouse.get_pressed()[0]:
                     lobby_1 = False
                     cred = True
+            if quitter.get_rect(topleft=(1000, 700)).collidepoint(mouse_pos):
+                window.blit(quitter_touched, (1000, 700))
+                if p.mouse.get_pressed()[0]:
+                    is_running = False
         
         if lobby_2:
             window.blit(background_image, (0, 0))
@@ -89,7 +96,8 @@ def main():
             pass
 
         if cred:
-            pass 
+            cred = False
+            lobby_1 = True
             #background = p.image.load("client/assets/bg_credits.png").convert()
             #background_image = p.transform.scale(background, (screen_width, screen_height))
         p.display.update()
