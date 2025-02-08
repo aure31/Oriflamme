a4c48ac91ad7a14725058db41c1fe4fa0dab6c9a
kedor <99351777+aure31@users.noreply.github.com>
2025-02-08 21:58:31 +0100

    remove des import de groupe element

--- client/groupelement.py
+++ client/groupelement.py
@@ -1,18 +1,9 @@
-import enum
-import loader as l
-from classes import Element,Bouton,TextInput,Texte, Chat
-import pygame
-import error as e
-import server.server as s
-import Joueur as j
-from network import Network, is_valid_ip, is_port
-
 class GroupElement:
     def __init__(self, name):
         self.name = name
         self.elements = {}
 
-    def affiche(self):
+    def affiche(self,window):
         for element in self.elements.values():
-            element.affiche(l.window)
+            element.affiche(window)
 
