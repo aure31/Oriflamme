ae937dfbd27425d2c207c1d533c4d8abc200c361
ArmandNSI <abonnementsarba@laposte.net>
2025-03-16 15:12:36 +0100

    filtre

--- client/loader.py
+++ client/loader.py
@@ -20,13 +20,28 @@ is_running = True
 game = None
 background = bg_menus
 
-gm = mots_bannis = [
-    "con", "connard", "connasse", "enculé", "salope", "pute", "fdp", "ntm",
-    "ta mère", "tg", "débile", "abruti", "crétin", "idiot", "imbécile",
-    "merde", "bordel", "chiant", "casse-couilles", "pétasse", "gouine",
-    "pd", "tapette", "enculer", "bâtard", "sous-merde", "raclure", "salaud",
-    "ducon", "duconne", "mongol", "autiste", "schizo", "dégénéré", "gros",
-    "moche", "nul", "suce", "branleur", "branleuse", "foutre", "nique", 
-    "suicide", "meurs", "crève", "tuer", "viol", "pédophile", "harcèlement",
-    "menace", "sale ", "clochard", "prostituée", " anus", "trouduc"
+mots_bannis = [
+    "con", "conne", "cons", "connes", "connard", "connarde", "connards", "connasses",
+    "enculé", "enculée", "enculés", "enculées", "salope", "salopes", "pute", "putes",
+    "fdp", "ntm", "nique", "niquer", "niqué", "niquée", "niqués", "niquées", 
+    "ta mère", "tm", "tg", "ta gueule", "ferme ta gueule", "débile", "débiles",
+    "abruti", "abrutie", "abrutis", "abruties", "crétin", "crétine", "crétins", "crétines",
+    "idiot", "idiote", "idiots", "idiotes", "imbécile", "imbéciles", "merde", "merdes",
+    "bordel", "bordels", "chiant", "chiante", "chiants", "chiantes", 
+    "casse-couilles", "casse burnes", "pétasse", "pétasses",
+    "gouine", "gouines", "pd", "tapette", "tapettes", "enculer", "enculés", "enculée",
+    "bâtard", "bâtarde", "bâtards", "bâtardes", "sous-merde", "raclure", "raclures", "salaud", "salauds",
+    "ducon", "duconne", "mongol", "mongole", "mongols", "mongoles",
+    "autiste", "autistes", "schizo", "schizophrène", "schizophrènes", "dégénéré", "dégénérée", "dégénérés", "dégénérées",
+    "gros", "grosse", "gros lard", "grosse vache", "gros tas", "moche", "moches",
+    "nul", "nulle", "nuls", "nulles", "suce", "sucer", "suceur", "suceuse", "suceurs", "suceuses",
+    "branleur", "branleuse", "branleurs", "branleuses", "branler", "branlé", "branlée", "branlés", "branlées",
+    "foutre", "se foutre", "foutre la merde", "nique ta mère", "suicide", "suicider", "suicidé", "suicidée", "suicidés", "suicidées",
+    "meurs", "mourir", "crève", "crèves", "crever", "tuer", "tue", "tué", "tuée", "tués", "tuées", "assassiner", "assassiné",
+    "viol", "violer", "violé", "violée", "violés", "violées", "violeur", "violeuse", "violeurs", "violeuses",
+    "pédophile", "pédophilie", "pédophiles", "harcèlement", "harceler", "harcelé", "harcelée", "harcelés", "harcelées",
+    "menace", "menacer", "menacé", "menacée", "menacés", "menacées",
+    "sale", "sales", "sale con", "sale connard", "sale connasse", "sale pute", "sale enculé",
+    "clochard", "clocharde", "clochards", "clochardes", "prostituée", "prostituées",
+    "anus", "trouduc", "trou du cul", "trous du cul"
 ]
